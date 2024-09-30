b16587c0cf8512fc7e03dca4d288714d3a6ba2d3
OG-1534 <gershomochanda@gmail.com>
2024-09-30 12:59:44 +0300

    installing Composer packages and correcting typo in database.php

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Load environment variables from .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR . '/../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 //Database credentials
