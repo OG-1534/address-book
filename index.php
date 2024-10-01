ec40f90aa06d48b7788306ce9232019a5740622f
OG-1534 <gershomochanda@gmail.com>
2024-10-01 07:59:36 +0300

    editing the index.php with error handlers

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
+// Include database connection
 include 'config/database.php';
 
+// Check if the connection is successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 // Fetch all entries from address_book
-$query = "SELECT address_book.*, cities.city_name FROM address_book JOIN cities ON address_book.city_id = cities.id";
+$query = "SELECT address_book.*, cities.city_name FROM address_book
+          JOIN cities ON address_book.city_id = cities.id";
 $result = $conn->query($query);
+
+// Check if query was successful
+if (!$result) {
+    die("Query failed: " . $conn->error);
+}
+
 ?>
 
 <h2>Address Book Entries</h2>
@@ -17,14 +30,14 @@ $result = $conn->query($query);
         <th>City</th>
         <th>Actions</th>
     </tr>
-    <?php while($row = $result->fetch_assoc()) { ?>
+    <?php while ($row = $result->fetch_assoc()) { ?>
         <tr>
-            <td><?php echo $row['name']; ?></td>
-            <td><?php echo $row['first_name']; ?></td>
-            <td><?php echo $row['email']; ?></td>
-            <td><?php echo $row['street']; ?></td>
-            <td><?php echo $row['zip_code']; ?></td>
-            <td><?php echo $row['city_name']; ?></td>
+            <td><?php echo htmlspecialchars($row['name']); ?></td>
+            <td><?php echo htmlspecialchars($row['first_name']); ?></td>
+            <td><?php echo htmlspecialchars($row['email']); ?></td>
+            <td><?php echo htmlspecialchars($row['street']); ?></td>
+            <td><?php echo htmlspecialchars($row['zip_code']); ?></td>
+            <td><?php echo htmlspecialchars($row['city_name']); ?></td>
             <td>
                 <a href="add_edit.php?id=<?php echo $row['id']; ?>">Edit</a>
                 <a href="process.php?delete=<?php echo $row['id']; ?>">Delete</a>
@@ -32,4 +45,11 @@ $result = $conn->query($query);
         </tr>
     <?php } ?>
 </table>
+
 <a href="add_edit.php">Add New Entry</a>
+
+<?php
+// Free the result set and close the connection
+$result->free();
+$conn->close();
+?>
